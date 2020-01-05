567da2dd3c75c8fbcac9326d1b7ff4c9505b96ad
Brian <briandsalemi@gmail.com>
2020-01-04 21:18:24 -0500

    initialized variable guess, added some more instructions for the player to respond to each of my guesses

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,10 +7,15 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     int max = 1000;
     int min = 1;
+
+    int guess;
+    
     void Start()
     {
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("Pick a number between " + min + "and " + max + " and I bet I can guess it!");
+        Debug.Log("Tell me if your number is higher or lower than guess.");
+        Debug.Log("Push Up Key if higher, Push Down if lower, and Push Enter if correct.");
     }
 
     // Update is called once per frame
