eac916ebaf15c6878ef0caac38dee8ee067dd66e
Brian <briandsalemi@gmail.com>
2020-01-05 09:46:19 -0500

    refactored some code by creating StartGame() and NextGuess() methods

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -12,37 +12,44 @@ public class NumberWizard : MonoBehaviour
     
     void Start()
     {
-        Debug.Log("Welcome to Number Wizard");
-        Debug.Log("Pick a number between " + min + "and " + max + " and I bet I can guess it!");
-        Debug.Log("Tell me if your number is higher or lower than " + guess);
-        Debug.Log("Push Up Key if higher, Push Down if lower, and Push Enter if correct.");
-
+       StartGame(); 
         
     }
 
+    void StartGame() 
+    {
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick a number between " + min + " and " + max + " and I bet I can guess it!");
+        Debug.Log("Is your number higher than, lower than, or exactly " + guess + "?");
+        Debug.Log("If it's higher, press Up. If it's lower, press Down. And if it's correct, press Enter.");
+
+        max = max + 1;
+
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow)) 
         {
-            Debug.Log("Up Arrow was pressed");
             min = guess;
-            Debug.Log(guess);
-            guess = (max + min) / 2;
+            NextGuess();
         }
                
         else if (Input.GetKeyDown(KeyCode.DownArrow)) 
         {
-            Debug.Log("Down Key was pressed");
             max = guess;
-            Debug.Log(guess);
-            guess = (max + min) / 2;
+            NextGuess();
         }
         
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Woohoo! I guessed correctly. Your number was: " + guess);
+            Debug.Log("Woohoo! I guessed correctly. Your number was " + guess + "!");
         }
             
+    }
+    void NextGuess() 
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is your number " + guess +"?");
     }
 }
