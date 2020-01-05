586c74684c6d224d78eb19fee4defc9832a19614
Brian <briandsalemi@gmail.com>
2020-01-04 21:08:49 -0500

    added max and min variables and interpolated them in the greeting and rules console logs

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class NumberWizard : MonoBehaviour
 {
     // Start is called before the first frame update
+    int max = 1000;
+    int min = 1;
     void Start()
     {
         Debug.Log("Welcome to Number Wizard");
-        Debug.Log("Pick a number between 1 and 1000 and I bet I can guess it!");
+        Debug.Log("Pick a number between " + min + "and " + max + " and I bet I can guess it!");
     }
 
     // Update is called once per frame
