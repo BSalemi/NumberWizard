563816e547e353784b95c2704e53bc56e46ce940
Brian <briandsalemi@gmail.com>
2020-01-04 20:47:52 -0500

    printed greetings and game rules to console. added script to the game object

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,7 +7,7 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Welcome to Number Wizard");
         Debug.Log("Pick a number between 1 and 1000 and I bet I can guess it!");
     }
 
